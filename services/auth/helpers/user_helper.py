b7caf5a594064b7af6fe6d675a825115d850d297
Осипов Артём <artemosipov307@gmail.com>
2025-12-11 21:53:20 +0300

    fix code user_helper

--- services/auth/helpers/user_helper.py
+++ services/auth/helpers/user_helper.py
@@ -6,8 +6,8 @@ from services.general.helpers.base_helper import BaseHelper
 class UserHelper(BaseHelper):
     ENDPOINT_PREFIX = "/users"
 
-    ME_ENDPOINT = f"/{ENDPOINT_PREFIX}/me/"
+    ME_ENDPOINT = f"{ENDPOINT_PREFIX}/me/"
 
     def get_me(self) -> requests.Response:
-        response = self.api_utils.post(self.ME_ENDPOINT)
+        response = self.api_utils.get(self.ME_ENDPOINT)
         return response
