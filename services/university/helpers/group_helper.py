cebeacfda5a2e47e0f621612a026a9ed9546777f
Осипов Артём <artemosipov307@gmail.com>
2025-12-14 00:37:13 +0300

    add api method group helper/api_utils

--- services/university/helpers/group_helper.py
+++ services/university/helpers/group_helper.py
@@ -11,3 +11,19 @@ class GroupHelper(BaseHelper):
     def post_group(self, json: dict) -> requests.Response:
         response = self.api_utils.post(self.ROOT_ENDPOINT, json=json)
         return response
+
+    def get_groups(self) -> requests.Response:
+        response = self.api_utils.get(self.ROOT_ENDPOINT)
+        return response
+
+    def get_group(self, path_param) -> requests.Response:
+        response = self.api_utils.get(self.ROOT_ENDPOINT + path_param + '/')
+        return response
+
+    def delete_group(self, path_param) -> requests.Response:
+        response = self.api_utils.delete(self.ROOT_ENDPOINT + path_param + '/')
+        return response
+
+    def update_group(self, path_param, json: dict) -> requests.Response:
+        response = self.api_utils.put(self.ROOT_ENDPOINT + path_param + '/', json=json)
+        return response
